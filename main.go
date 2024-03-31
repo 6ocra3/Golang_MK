fa265b205dc09d8d907402a0916a757be506eff0
6ocra3 <6o3kmm@mail.ru>
2024-04-01 00:41:56 +0300

    make fixes

--- main.go
+++ main.go
@@ -3,61 +3,72 @@ package main
 import (
 	"flag"
 	"fmt"
+	"slices"
 	"strings"
+	"unicode"
 
 	"github.com/jdkato/prose/v2"
 	"github.com/kljensen/snowball"
 )
 
 func main() {
-
-	sFlag := flag.String("s", "", "Флаг `-s` используется для ввода строки")
+	var input string
+	flag.StringVar(&input, "s", "", "Флаг `-s` используется для ввода строки")
 	flag.Parse()
 
-	if *sFlag != "" {
-
-		input := *sFlag
-
-		// Кусок кода, который обрабатывает строку, выделяет слова и характеризует каждое слово
-		doc, err := prose.NewDocument(input)
-		if err != nil {
-			fmt.Println("Can't preprocess input for prose:", err)
-		}
-
-		appended := make(map[string]bool)
-		var filtered []string
-		for _, tok := range doc.Tokens() {
-			if checkWord(tok) {
-
-				// Обработка слова
-				stemmed, err := snowball.Stem(tok.Text, "english", true)
-				if err != nil {
-					fmt.Println("can't stem word:", err)
-					continue
-				}
-
-				// Добавление в итоговый ответ только уникальных слов
-				if _, ok := appended[stemmed]; !ok {
-					filtered = append(filtered, stemmed)
-					appended[stemmed] = true
-				}
-
-			}
-		}
-		result := strings.Join(filtered, " ")
-		fmt.Println(result)
-	} else {
+	if input == "" {
 		fmt.Println(`На вход не подана строка. Используйте -s и "" `)
+		return
 	}
+
+	stemmedInput := stemmString(input)
+
+	result := strings.Join(stemmedInput, " ")
+	fmt.Println(result)
+}
+
+// Функция для стемминга
+func stemmString(input string) []string {
+	// Удаление знаков препинания
+	input = strings.Map(func(r rune) rune {
+		if unicode.IsPunct(r) {
+			return -1
+		}
+		return r
+	}, input)
+
+	// Кусок кода, который обрабатывает строку, выделяет слова и характеризует каждое слово
+	doc, err := prose.NewDocument(input)
+	if err != nil {
+		fmt.Println("Can't preprocess input for prose:", err)
+	}
+
+	appended := make(map[string]bool)
+	var filtered []string
+	for _, tok := range doc.Tokens() {
+		if checkWord(tok) {
+
+			// Обработка слова
+			stemmed, err := snowball.Stem(tok.Text, "english", true)
+			if err != nil {
+				fmt.Println("can't stem word:", err)
+				continue
+			}
+
+			// Добавление в итоговый ответ только уникальных слов
+			if !appended[stemmed] {
+				filtered = append(filtered, stemmed)
+				appended[stemmed] = true
+			}
+
+		}
+	}
+
+	return filtered
 }
 
 func checkWord(tok prose.Token) bool {
-	// forbidden_tags - тэги, который будут только у местоимений, чатсиц, предлогов и союзов
-	forbidden_tags := []string{"DT", "IN", "CC", "PRP", "PRP$"}
-	for _, token_code := range forbidden_tags {
-		if tok.Tag == token_code {
-			return false
-		}
-	}
-	return true
+	// forbiddenTags - тэги, который будут только у местоимений, чатсиц, предлогов и союзов
+	forbiddenTags := []string{"DT", "IN", "CC", "PRP", "PRP$"}
+	return !slices.Contains(forbiddenTags, tok.Tag)
 }
